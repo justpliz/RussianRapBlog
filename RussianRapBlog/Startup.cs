f4acb3d4efd8dfc58d26bf4353bb58070f3df2c9
DarksideF41 <72594123+DarksideF41@users.noreply.github.com>
2021-06-07 22:50:01 +0700

    Чистка

--- RussianRapBlog/Startup.cs
+++ RussianRapBlog/Startup.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Database;
-using Microsoft.EntityFrameworkCore;
 using Services;
 
 namespace RussianRapBlog
@@ -19,12 +13,11 @@ namespace RussianRapBlog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var con = @"Server=(localdb)\mssqllocaldb;Database=BlogDb;Trusted_Connection=True;"; //TODO вынести
             services.AddControllers();
             services.AddMvc();
             services.AddSwaggerGen();
-            services.AddDbContext<RussianRapBlogContext>(options => options.UseSqlServer(con));
-            services.AddTransient<IPostService, PostService>();
+            services.AddDbContext<RussianRapBlogContext>();
+            services.AddScoped<IPostService, PostService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
